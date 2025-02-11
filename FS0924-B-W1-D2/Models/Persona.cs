80696da5122d829c3073b114e4c6b9997d27fc61
Ninfa-C <ninfa_95@hotmail.it>
2025-02-11 14:41:16 +0100

    Aggiungere i file di progetto.

---
+++ FS0924-B-W1-D2/Models/Persona.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FS0924_B_W1_D2.Models
+{
+    public class Persona
+    {
+        public string? Nome { get; set; }
+        public string? Cognome { get; set; }
+        public int Età { get; set; }
+
+        public Persona(string nome, string cognome, int età)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Età = età;
+        }
+
+        // metodi GetNome, GetCognome, GetEta e GetDettagli che restituisca in una stringa le informazioni sulla persona in questione.
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public string GetCognome()
+        {
+            return Cognome;
+        }
+
+        public int GetEta()
+        {
+            return Età;
+        }
+
+        public string GetDettagli()
+        {
+            return $"Nome: {Nome}, Cognome: {Cognome}, Età: {Età}";
+        }
+
+    }
+}
---
+++ FS0924-B-W1-D2/Program.cs
@@ -0,0 +1,9 @@
+﻿using FS0924_B_W1_D2;
+using FS0924_B_W1_D2.Models;
+
+Persona persona = new("Mario", "Rossi", 35);
+
+Console.WriteLine("Nome: " + persona.GetNome());
+Console.WriteLine("Cognome: " + persona.GetCognome());
+Console.WriteLine("Età: " + persona.GetEta());
+Console.WriteLine("Dettagli: " + persona.GetDettagli());
